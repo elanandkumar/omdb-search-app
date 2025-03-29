5839bb70f95e77b5f4767cce1421b121ce6e2d2e
Anand Kumar <anand.kumar@el-anand-kumar.local>
2025-03-29 21:44:50 +0530

    Code refactored for better reusabilit and separation of concern

--- js/main.js
+++ js/main.js
@@ -1,27 +1,82 @@
-document.getElementById('search_btn').addEventListener('click', function(){
-	var title = document.getElementById('title_text').value;
-	var request = new Request('https://www.omdbapi.com/?apikey=4dd3fc90&s='+title);
-	fetch(request).then(function(result){
-		return result.json();
-	}).then(function(data){
-		var searchEl = document.getElementById('search_result');
-		var len = data.Search.length;
-		for(var i = 0; i < len; i++){
-			var movieContainer = document.createElement('div');
-			movieContainer.className = 'search-result--item';
-			var titleEl = document.createElement('div');
-			titleEl.innerText = data.Search[i].Title;
-			var yearEl = document.createElement('div');
-			yearEl.innerText = data.Search[i].Year;
-			var typeEl = document.createElement('div');
-			typeEl.innerText = data.Search[i].Type;
-			var posterEl = document.createElement('img');
-			posterEl.src = data.Search[i].Poster;
-			movieContainer.appendChild(posterEl);
-			movieContainer.appendChild(titleEl);
-			movieContainer.appendChild(yearEl);
-			movieContainer.appendChild(typeEl);
-			searchEl.appendChild(movieContainer);
-		}
-	});
-});
+let loader, searchBtn, searchResult;
+
+function toggleLoader(show) {
+  loader.style.display = show ? "block" : "none";
+  searchResult.style.display = show ? "none" : "inline-block";
+}
+
+async function fetchData(url) {
+  const response = await fetch(url);
+  return await response.json();
+}
+
+async function doSearch(searchTerm) {
+  const url = new Request(
+    "https://www.omdbapi.com/?apikey=4dd3fc90&s=&" + searchTerm
+  );
+
+  return await fetchData(url);
+}
+
+function getMovieContainer(card) {
+  const movieContainer = document.createElement("div");
+  movieContainer.className = "search-result--item";
+  const titleEl = document.createElement("div");
+  titleEl.innerText = card.Title;
+  const yearEl = document.createElement("div");
+  yearEl.innerText = card.Year;
+  const typeEl = document.createElement("div");
+  typeEl.innerText = card.Type;
+  const posterEl = document.createElement("img");
+  posterEl.src = card.Poster;
+
+  movieContainer.appendChild(posterEl);
+  movieContainer.appendChild(titleEl);
+  movieContainer.appendChild(yearEl);
+  movieContainer.appendChild(typeEl);
+
+  return movieContainer;
+}
+
+function renderCards(cards) {
+  cards.forEach((card) => {
+    const movieContainer = getMovieContainer(card);
+
+    searchResult.appendChild(movieContainer);
+  });
+}
+
+async function handleSearch() {
+  searchResult.innerHTML = "";
+  toggleLoader(true);
+  const searchTerm = document.getElementById("title_text").value;
+  try {
+    const searchResponse = await doSearch(searchTerm);
+    if (searchResponse.Response === "False") {
+      searchResult.innerHTML = "Something went wrong!!!";
+      toggleLoader(false);
+    } else {
+      var len = searchResponse?.Search?.length ?? 0;
+      if (len) {
+        renderCards(searchResponse.Search);
+        toggleLoader(false);
+      } else {
+        searchResult.innerHTML = "No results found!!!";
+        toggleLoader(false);
+      }
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function init() {
+  loader = document.getElementById("loader");
+  searchBtn = document.getElementById("search_btn");
+  searchResult = document.getElementById("search_result");
+  searchBtn.onclick = handleSearch;
+
+  toggleLoader(false);
+}
+
+init();
